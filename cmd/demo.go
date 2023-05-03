e0720687f5d28cd0616df36630a3cb70426a202c
Rudi Farkas <rudi.farkas@gmail.com>
2023-05-04 00:32:38 +0200

    add tidwall/gjson, Demo1, Demo2

--- cmd/demo.go
+++ cmd/demo.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/tidwall/gjson"
 )
 
 // demoCmd represents the demo command
@@ -16,6 +17,8 @@ var demoCmd = &cobra.Command{
 	Long:  `Examples of gjson usage from gjson repo`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("demo called")
+		Demo1()
+		Demo2()
 	},
 }
 
@@ -31,4 +34,26 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// demoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func Demo1() {
+	const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
+	value := gjson.Get(json, "name.last")
+	println("1", value.String())
+}
+
+func Demo2() {
+	const json = `{
+  "name": {"first": "Tom", "last": "Anderson"},
+  "age":37,
+  "children": ["Sara","Alex","Jack"],
+  "fav.movie": "Deer Hunter",
+  "friends": [
+    {"first": "Dale", "last": "Murphy", "age": 44, "nets": ["ig", "fb", "tw"]},
+    {"first": "Roger", "last": "Craig", "age": 68, "nets": ["fb", "tw"]},
+    {"first": "Jane", "last": "Murphy", "age": 47, "nets": ["ig", "tw"]}
+  ]
+}`
+	value := gjson.Get(json, "name.last")
+	println("2", value.String())
 }
